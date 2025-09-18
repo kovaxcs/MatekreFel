f5a9dd93fa90f5379d7b0d37214045af9d0fbec5
Csongor Kovacs <kovax2004@yahoo.com>
2025-09-18 12:21:53 +0300

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(cors({
 }));
 
 app.get('/', (req, res) => { 
-  res.send("Teszt12");
+  res.send("TesztV");
 });
 
 app.post('/generate-pdf', async (req, res) => {
@@ -33,19 +33,19 @@ app.post('/generate-pdf', async (req, res) => {
   res.set('Access-Control-Allow-Methods', 'POST');
   res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   const html = req.body;
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+  //const browser = await puppeteer.launch();
+  //const page = await browser.newPage();
 
   // Load your HTML and wait for assets
-  await page.setContent(html, { waitUntil: 'networkidle0' });
+  //await page.setContent(html, { waitUntil: 'networkidle0' });
 
   // Wait for MathJax if you have LaTeX. Optionally, wait for a JS flag set after rendering:
   //await page.waitForFunction('window.MathJax && MathJax.typesetPromise', {timeout: 5000}).catch(()=>{});
   //await page.evaluate(() => { if(window.MathJax) { return window.MathJax.typesetPromise(); } });
 
   // Generate PDF
-  const pdfBuffer = await page.pdf({ format: 'A4', printBackground: true });
-  await browser.close();
+  //const pdfBuffer = await page.pdf({ format: 'A4', printBackground: true });
+  //await browser.close();
 
   //res.set({'Content-Type': 'application/pdf'});
   res.send(html);
